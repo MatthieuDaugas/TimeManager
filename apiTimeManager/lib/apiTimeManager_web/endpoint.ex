af6b25e442d38059e3a53ec9dfbcd8f4ce1b25a6
fmo-dev <ftmn.dev@gmail.com>
2020-10-21 23:58:37 +0200

    pages WorkingTimes & WorkingTime

--- apiTimeManager/lib/apiTimeManager_web/endpoint.ex
+++ apiTimeManager/lib/apiTimeManager_web/endpoint.ex
@@ -10,44 +10,48 @@ defmodule ApiTimeManagerWeb.Endpoint do
     signing_salt: "IiNfumE5"
   ]
 
-  socket "/socket", ApiTimeManagerWeb.UserSocket,
+  socket("/socket", ApiTimeManagerWeb.UserSocket,
     websocket: true,
     longpoll: false
+  )
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :apiTimeManager,
     gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :apiTimeManager
+    plug(Phoenix.CodeReloader)
+    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :apiTimeManager)
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
+  plug(Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
+  )
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
-  plug Plug.Session, @session_options
-  plug Corsica, origins: "*"
-  plug ApiTimeManagerWeb.Router
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(Corsica, origins: "*", allow_headers: ["content-type"])
+  plug(ApiTimeManagerWeb.Router)
 end
--- webTimeManager/web-time-manager/src/router/index.js
+++ webTimeManager/web-time-manager/src/router/index.js
@@ -21,7 +21,13 @@ const routes = [
   {
     path: "/workingtime",
     name: "WorkingTime",
-    component: WorkingTime
+    component: WorkingTime,
+    props: (route) => {
+      return {  
+        workingTime: route.params.element,
+        ...route.params
+      }
+    }
   },
   {
     path: "/workingtimes",
