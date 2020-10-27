2edd61075b8ebf6fc01df946ed81d3e9a317d925
NUNES <thomas.nunes@epitech.eu>
2020-10-27 15:28:17 +0000

    Docker & Mix

--- apiTimeManager/config/test.exs
+++ apiTimeManager/config/test.exs
@@ -15,7 +15,7 @@ config :apiTimeManager, ApiTimeManager.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :apiTimeManager, ApiTimeManagerWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 4000],
   server: true
 
 # Print only warnings and errors during test
